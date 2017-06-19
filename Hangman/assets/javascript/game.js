2c1a7acbd4a851387faa3d2d8e33f5b281f01e04
Alex <alexanderbullock87@gmail.com>
2017-06-18 22:42:12 -0400

    optimization

--- Hangman/assets/javascript/game.js
+++ Hangman/assets/javascript/game.js
@@ -1,22 +1,17 @@
-//Object definitions
-
-//word constructor--each word has a string and a sound
-
-
-
+//Object definition
 var game = {
 
 	//will act as a word constructor
-	makeWord: function(word, sound){
+	makeWord: function(word, sound, hint){
 		this.word=word;
 		this.sound=sound;
+		this.hint = hint;
 	},
 	
-	//Array of possible words
+	//Array of possible words--will hold word objects
 	words:[],
 
-
-	currentWord:"",
+	currentIndex:0,
 
 	//Array to contain which letters have been guessed
 	lettersGuessed:[],
@@ -41,7 +36,7 @@ var game = {
 				result.push("_");
 			}
 			else
-				result.push("&nbsp;")
+				result.push("&nbsp;&nbsp;&nbsp;")
 		}
 
 		return result;
@@ -49,16 +44,18 @@ var game = {
 
 	//Function to pick a random word from the Words array
 	//accepts an array, returns a random element
+	//updates the callers current index
 	PickWord: function(words){
-		return words[Math.floor(Math.random()*words.length)].word;
+		this.currentIndex = Math.floor(Math.random()*words.length);
 	},
 
 	//Function to start new game
 	//picks new word, updates scores, resets guessed letters
 	StartNewGame: function(){
-		this.currentWord=this.PickWord(this.words);
-		this.gameString = this.ConvertWordToGame(this.currentWord);
+		this.PickWord(this.words);
+		this.gameString = this.ConvertWordToGame(this.words[this.currentIndex].word);
 		document.getElementById("GuessesLeftId").style="color:white;"
+		document.getElementById("HintId").innerHTML=this.words[this.currentIndex].hint;
 		this.guessesLeft=10;
 		this.lettersGuessed=[];
 	},
@@ -71,30 +68,22 @@ var game = {
 		var isCorrectGuess = false;
 
 		//Do nothing if user already guessed this letter
-		for(var i=0; i<this.lettersGuessed.length;i++){
-			if(guess===this.lettersGuessed[i]){
-						
-				return;
-			}
-		}
-		//Do nothing if user already guessed this letter
-		for(var i=0; i<this.gameString.length; i++){
-			if(guess===this.gameString[i]){
-						
-				return;
-			}
-		}
-
+		if(this.lettersGuessed.includes(guess) || this.gameString.includes(guess))
+			return;
+		
 		//determine if correct/incorrect guess
 		for(var i = 0; i < currWord.length; i++){
 			if(guess === currWord[i]){
+				//update game string if correct
 				this.gameString[i] = guess;
 				isCorrectGuess = true;
 			}
 		}
+		
 		//update game object accordingly
 		if(!isCorrectGuess){
 			this.guessesLeft--;
+			//warn user when 3 guesses remaining
 			if(this.guessesLeft<4){
 				document.getElementById("GuessesLeftId").style="color:red;"
 			}
@@ -109,36 +98,20 @@ var game = {
 
 	},
 	
-
-
-
-	PlayWinningSong: function(word, words){
-		for(var i =0; i< this.words.length; i++){
-			if(this.words[i].word===word)
-				this.words[i].sound.play();
-		}
-	},
-	//checks if a user has won
+	//checks if a user has won, updates game object accordingly
 	CheckWin: function(){
 		
-		for(var i = 0; i < this.gameString.length; i++){
-			if(this.gameString[i]==="_"){
-				return false;
-			}
+		if(!this.gameString.includes("_")){
+			this.gamesWon++;
+			this.words[this.currentIndex].sound.play();
+			this.StartNewGame();
 		}
-		this.gamesWon++;
-		
-		this.PlayWinningSong(this.currentWord, this.words);
-		
-		this.StartNewGame();
-		return true;
 	},
 
 	//Updates the screen with the "game" object's values
 	UpdateScreen: function(){
 		document.getElementById("GamesWonId").innerHTML = this.gamesWon;
 		document.getElementById("GamesLostId").innerHTML = this.gamesLost;
-
 		document.getElementById("GameBoardId").innerHTML = this.gameString.join("  ");
 		document.getElementById("LettersGuessedId").innerHTML = this.lettersGuessed;
 		document.getElementById("GuessesLeftId").innerHTML = this.guessesLeft;
@@ -148,13 +121,16 @@ var game = {
 
 
 }
-	game.words=[new game.makeWord("nicki minaj", new Audio('assets/sounds/Nicki - Minaj.mp3')), new game.makeWord("outkast", new Audio('assets/sounds/Big Boi From Outkast - Big Boi From Outkast.mp3')), new game.makeWord("travis scott", new Audio('assets/sounds/Travis Scott - Intro sound signature.mp3')), new game.makeWord("desiigner", new Audio('assets/sounds/Desiigner - Desiigner.mp3')), new game.makeWord("migos", new Audio('assets/sounds/Migos - Raindrop Drop Top Sound Effect.mp3')), new game.makeWord("madeintyo", new Audio('assets/sounds/Made - In.mp3')), new game.makeWord("lil wayne", new Audio('assets/sounds/Lil - Wayne.mp3')), new game.makeWord("kanye west", new Audio('assets/sounds/Kanye West - Kanye West.mp3')), new game.makeWord("rick ross", new Audio('assets/sounds/Rick - Ross.mp3')), new game.makeWord("french montana", new Audio('assets/sounds/french - montana.mp3')), new game.makeWord("gucci mane", new Audio('assets/sounds/Gucci - Mane.mp3')), new game.makeWord("nas", new Audio('assets/sounds/Nas - Saying.mp3')),new game.makeWord("lil uzi vert", new Audio('assets/sounds/Lil Uzi Vert - It Do Not Matter Sound Effect.mp3')),new game.makeWord("kodak black", new Audio('assets/sounds/Kodak Black - The Finesse Kid Sound Effect.mp3')), new game.makeWord("eminem", new Audio('assets/sounds/Eminem - Slim.mp3')),new game.makeWord("big sean", new Audio('assets/sounds/Big - Sean.mp3')),new game.makeWord("jay z", new Audio('assets/sounds/JAY - Z.mp3')),new game.makeWord("lil jon", new Audio('assets/sounds/LIL - JON.mp3')),new game.makeWord("notorious big",new Audio('assets/sounds/BIGGIE - AND.mp3')),new game.makeWord("dj khaled",new Audio('assets/sounds/DJ - Khaled.mp3'))];
+//create array of word objects inside of the game object
+game.words=[new game.makeWord("nicki minaj", new Audio('assets/sounds/Nicki - Minaj.mp3'), "Guess The Artist"), new game.makeWord("outkast", new Audio('assets/sounds/Big Boi From Outkast - Big Boi From Outkast.mp3'), "Guess The Artist"), new game.makeWord("travis scott", new Audio('assets/sounds/Travis Scott - Intro sound signature.mp3'), "Guess The Artist"), new game.makeWord("desiigner", new Audio('assets/sounds/Desiigner - Desiigner.mp3'), "Guess The Artist"), new game.makeWord("bad and boujee", new Audio('assets/sounds/Migos - Raindrop Drop Top Sound Effect.mp3'), "Guess The Song"), new game.makeWord("madeintyo", new Audio('assets/sounds/Made - In.mp3'), "Guess The Artist"), new game.makeWord("lil wayne", new Audio('assets/sounds/Lil - Wayne.mp3'), "Guess The Artist"), new game.makeWord("kanye west", new Audio('assets/sounds/Kanye West - Kanye West.mp3'), "Guess The Artist"), new game.makeWord("rick ross", new Audio('assets/sounds/Rick - Ross.mp3'), "Guess The Artist"), new game.makeWord("french montana", new Audio('assets/sounds/french - montana.mp3'), "Guess The Artist"), new game.makeWord("gucci mane", new Audio('assets/sounds/Gucci - Mane.mp3'), "Guess The Artist"), new game.makeWord("stillmatic", new Audio('assets/sounds/Nas - Saying.mp3'), "Guess The Album"),new game.makeWord("money longer", new Audio('assets/sounds/Lil Uzi Vert - It Do Not Matter Sound Effect.mp3'), "Guess The Song"),new game.makeWord("kodak black", new Audio('assets/sounds/Kodak Black - The Finesse Kid Sound Effect.mp3'), "Guess The Artist"), new game.makeWord("the slim shady lp", new Audio('assets/sounds/Eminem - Slim.mp3'), "Guess The Album"),new game.makeWord("big sean", new Audio('assets/sounds/Big - Sean.mp3'), "Guess The Artist"),new game.makeWord("the black album", new Audio('assets/sounds/JAY - Z.mp3'), "Guess The Album"),new game.makeWord("lil jon", new Audio('assets/sounds/LIL - JON.mp3'), "Guess The Artist"),new game.makeWord("life after death",new Audio('assets/sounds/BIGGIE - AND.mp3'), "Guess The Album"),new game.makeWord("dj khaled",new Audio('assets/sounds/DJ - Khaled.mp3'), "Guess The Artist")];
+
 //when the page is loaded, the landing page is displayed and the game page isn't
 document.getElementById("GamePage").style.display="none";
 document.getElementById("LandingPage").style.display="block";
 
 //defining the function that exectues onkeyup
 document.onkeyup = function(event){
+
 	//If we are on landing page, switch to game page
 	if(document.getElementById("GamePage").style.display=="none"){
 		document.getElementById("GamePage").style.display="block";
@@ -163,9 +139,12 @@ document.onkeyup = function(event){
 		game.UpdateScreen();
 		return;
 	}
+
 	//Only accept keys from a-z ignoring case
 	if(/[a-z]/i.test(String.fromCharCode(event.keyCode))){
-		game.CheckGuess(event.key, game.currentWord);
+		//for every valid key, check the guess, check if they won, 
+		//update the game object, and update the screen.
+		game.CheckGuess(event.key, game.words[game.currentIndex].word);
 		game.CheckWin(game.GameString);
 		game.UpdateScreen();
 	}
